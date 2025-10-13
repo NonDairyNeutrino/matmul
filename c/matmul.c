4e6db009e76241d5d1bebc9575a21f7f9393f4e7
nondairyneutrino <chapmann19@gmail.com>
2025-10-13 13:20:54 -0700

    begin multithreading

--- c/matmul.c
+++ c/matmul.c
@@ -2,18 +2,19 @@
 // Multithreaded matrix multiplication using new C-standard threading
 
 #include <stdlib.h>
+#include <threads.h>
 #include "print.h"
 #include "ops.h"
-
-int square(int x) {
-  return x*x;
-}
 
 int main(int argc, char* argv[]) {
   int dim = atoi(argv[1]);
   int fill = atoi(argv[2]);
   int mat[dim][dim];
   int matsqrd[dim][dim];
+
+  // first create a thread pool so they don't have to be created and destoryed
+  // on the fly
+  thrd_t tp[dim];
 
   fill_matrix(dim, mat, fill);
   sqrmatmul(dim, mat, mat, matsqrd);
--- c/ops.c
+++ c/ops.c
@@ -3,6 +3,7 @@
 #if DEBUG
   #include <stdio.h>
 #endif
+#include <threads.h>
 #include <stdlib.h>
 #include "ops.h"
 
@@ -47,20 +48,22 @@ void rand_matrix(int dim, int mat[dim][dim]) {
   }
 }
 
+// take the dot product of two vectors
+int dot(int dim, int vecA[dim], int vecB[dim]) {
+  int dp = 0;
+  for (int i = 0; i < dim; i++) {
+    dp += vecA[i] * vecB[i];
+  }
+  return dp;
+}
+
 // multiply two matrices and store the result in a given matrix
 void matmul(int dimA, int matA[dimA][dimA], int dimB, int matB[dimB][dimB], int matC[dimA][dimB]) {
   for (int i = 0; i < dimA; i++) {
     for (int j = 0; j < dimB; j++) {
       matC[i][j] = 0; // make sure to zero out the result matrix
-      for (int k = 0; k < dimB; k++) {
-        matC[i][j] += matA[i][k] * matB[k][j];
-        #if DEBUG
-          printf("matA[%d][%d] = %d\n", i, k, matA[i][k]);
-          printf("matB[%d][%d] = %d\n", i, k, matA[k][j]);
-          printf("matC[%d][%d] = %d\n", i, j, matC[i][j]);
-          printf("\n");
-        #endif
-      }
+      int* row = matA[i];
+      int* col = matB[][j] // TODO: add matrix transpose
     }
   }
 }
@@ -69,3 +72,5 @@ void matmul(int dimA, int matA[dimA][dimA], int dimB, int matB[dimB][dimB], int 
 void sqrmatmul(int dim, int matA[dim][dim], int matB[dim][dim], int matC[dim][dim]) {
   matmul(dim, matA, dim, matB, matC);
 }
+
+
